21b546a60f85bd25191d75354abd91e5b37065f3
ildar2244 <ildar2244@gmail.com>
2015-07-22 14:00:40 +0300

    42

--- app/src/main/java/ru/javaapp/openeventmaterial/activities/AddEventsActivity.java
+++ app/src/main/java/ru/javaapp/openeventmaterial/activities/AddEventsActivity.java
@@ -36,11 +36,6 @@ public class AddEventsActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         if (id == android.R.id.home) {
             finish();
             return true;
